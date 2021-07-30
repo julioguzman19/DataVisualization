232c4359d0bb74e822d88564311a4dacd9d576ae
Julio <julioguzman19@gmail.com>
2021-07-29 23:00:05 -0600

    CSV test data file saved

---
+++ index.js
@@ -0,0 +1,12 @@
+$.ajax({
+    url: "stats.csv",
+    async: false,
+    success: function (csvd) {
+        data = $.csv.toArrays(csvd);
+    },
+    dataType: "text",
+    complete: function () {
+        // call a function on complete 
+        console.log(data);
+    }
+});
