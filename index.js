0ddf2c2aaf3857ce1a06da3c48c0de4c1ef6d359
Julio <julioguzman19@gmail.com>
2021-07-30 22:14:47 -0600

    Removing columns from excel file

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+let globalData = [];
+
 window.onload = async function(){
     d3.csv("https://raw.githubusercontent.com/julioguzman19/DataVisualization/main/stats.csv", function(data){
-        console.log(data);
+    globalData = data;  
+    printData(data);
     });
+}
+
+function printData(data){
+    console.log(data);
 }
