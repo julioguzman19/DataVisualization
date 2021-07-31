d0e05321a14a58fc3467f46543c191f2878b6a34
Julio <julioguzman19@gmail.com>
2021-07-31 11:48:24 -0600

    Reading names column into array

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
+
+let names = []; 
 let globalData = [];
+let i = 0;
 
 window.onload = async function(){
     d3.csv("https://raw.githubusercontent.com/julioguzman19/DataVisualization/main/stats.csv", function(data){
-    globalData = data;  
-    printData(data);
+    
+    // Data being read one row at a time
+    globalData[i] = data;  
+
+    // Increment
+    i++;
     });
 }
 
-function printData(data){
-    console.log(data);
+function printData(){
+    createNamesArray();
+    //console.log(names[0]);
+}
+
+function createNamesArray(){
+    for(let i = 0; i < globalData.length; i++){
+        //console.log(globalData[i].NAME);
+        names.push(globalData[i].NAME);
+    }
 }
