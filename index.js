6a493fa8e41f1892f2688664cfdcd04a60ac248f
Julio <julioguzman19@gmail.com>
2021-08-01 09:53:16 -0600

    Sort function successful

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 // Initializing Global Arrays to store data
 let globalData = [];
-// let headersArr = ["NAME", "TEAM", "PPG", "RPG", "APG", "SPG", "BPG", "TOPG"];
-// let namesArr = [];
-// let ppgArr = [];
-// let rpgArr = [];
-// let apgArr = [];
-// let spgArr = [];
-// let bpgArr = [];
-// let topgArr = [];
-let dataObj = [];
+let dataArr = [];
+let userSelection;
 
 // Initializing iterations for window on load
 let i = 0;
@@ -25,11 +18,9 @@ window.onload = async function () {
     });
 }
 
-function printData() {
-    createArrays();
-    //console.log(dataObj.namesArr[0]);
+function createVisual() {
+    createDataArray();
 
-    //
     // set the dimensions and margins of the graph
     var margin = { top: 10, right: 30, bottom: 40, left: 100 },
         width = 460 - margin.left - margin.right,
@@ -45,10 +36,10 @@ function printData() {
             "translate(" + margin.left + "," + margin.top + ")");
 
     // sort data
-    dataObj.sort(function (b, a) {
-        return a.apg - b.apg;
-    });
-
+    // dataArr.sort(function (b, a) {
+    //     return a.apg - b.apg;
+    // });
+    sortDataArray();
 
     // Add X axis
     var x = d3.scaleLinear()
@@ -65,14 +56,14 @@ function printData() {
     // Y axis
     var y = d3.scaleBand()
         .range([0, height])
-        .domain(dataObj.map(function (d) { return d.name; }))
+        .domain(dataArr.map(function (d) { return d.name; }))
         .padding(1);
     svg.append("g")
         .call(d3.axisLeft(y))
 
     // Lines
     svg.selectAll("myline")
-        .data(dataObj)
+        .data(dataArr)
         .enter()
         .append("line")
         .attr("x1", function (d) { return x(d.apg); })
@@ -83,31 +74,23 @@ function printData() {
 
     // Circles
     svg.selectAll("mycircle")
-        .data(dataObj)
+        .data(dataArr)
         .enter()
         .append("circle")
         .attr("cx", function (d) { return x(d.apg); })
         .attr("cy", function (d) { return y(d.name); })
         .attr("r", "7")
-        .style("fill", "#69b3a2")
+        .style("fill", "#FF7F7F")
         .attr("stroke", "black")
 
 
 
 }
 
-function createArrays() {
-    let name,ppg,rpg,apg,spg,bpg,topg;
+function createDataArray() {
+    let name, ppg, rpg, apg, spg, bpg, topg;
 
     for (let i = 0; i < globalData.length; i++) {
-        //Pushing csv column data into arrays
-        // namesArr.push(globalData[i].NAME);
-        // ppgArr.push(globalData[i].PPG);
-        // rpgArr.push(globalData[i].RPG);
-        // apgArr.push(globalData[i].APG);
-        // spgArr.push(globalData[i].SPG);
-        // bpgArr.push(globalData[i].BPG);
-        // topgArr.push(globalData[i].TOPG);
 
         // Creating temp vars
         name = globalData[i].NAME;
@@ -116,11 +99,58 @@ function createArrays() {
         apg = globalData[i].APG;
         spg = globalData[i].SPG;
         bpg = globalData[i].BPG;
-        topg = globalData[i].TOPG; 
+        topg = globalData[i].TOPG;
 
         // Adding temp vars to objects
-        dataObj[i] = {name:name,ppg:ppg,rpg:rpg,apg:apg,spg:spg,bpg,bpg,topg:topg};
+        dataArr[i] = { name: name, ppg: ppg, rpg: rpg, apg: apg, spg: spg, bpg, bpg, topg: topg };
 
     }
-    
+
+}
+
+function sortDataArray() {
+    // Get user selection
+    userSelection = document.getElementById("stats").value;
+
+    // Sort based on userSelection
+    switch (userSelection) {
+        case "PointsPerGame":
+            // Sort 
+            dataArr.sort(function (b, a) {
+                return a.ppg - b.ppg;
+            });
+
+            break;
+        case "ReboundsPerGame":
+            // Sort 
+            dataArr.sort(function (b, a) {
+                return a.rpg - b.rpg;
+            });
+            break;
+        case "AssistsPerGame":
+            // Sort 
+            dataArr.sort(function (b, a) {
+                return a.apg - b.apg;
+            });
+            break;
+        case "StealsPerGame":
+            // Sort 
+            dataArr.sort(function (b, a) {
+                return a.spg - b.spg;
+            });
+            break;
+        case "BlocksPerGame":
+            // Sort 
+            dataArr.sort(function (b, a) {
+                return a.bpg - b.bpg;
+            });
+            break;
+        case "TurnoversPerGame":
+            // Sort 
+            dataArr.sort(function (b, a) {
+                return a.topg - b.topg;
+            });
+    }
+
+
 }
